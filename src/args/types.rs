50c3bc77b0d2df962cd08111780ce2bae63daef9
OSCH2008 <82794982+OSCH2008@users.noreply.github.com>
2024-03-17 16:50:07 -0800

    add help text

---
+++ src/args/types.rs
@@ -0,0 +1,30 @@
+pub enum VideoCodec {
+    h264, av1, vp9
+}
+impl Default for VideoCodec {
+    fn default() -> Self {
+        Self::h264
+    }
+}
+
+pub struct VideoQuality {
+    quality: u16
+}
+impl Default for VideoQuality {
+    fn default() -> Self {
+        Self { quality: 1080 }
+    }
+}
+
+pub enum AudioFormat {
+    BEST, MP3, OGG, WAV, OPUS
+}
+impl Default for AudioFormat {
+    fn default() -> Self {
+        Self::MP3
+    }
+}
+
+pub enum Help {
+    Get, List, Bulk
+}
