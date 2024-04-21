43f4cd262e7541f2ee89b0a6a49f587ce939ff7f
khyernet <khyernet@vivaldi.net>
2024-04-20 17:16:08 -0800

    implement main get method successfully

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::process::ExitCode {
         return std::process::ExitCode::SUCCESS;
     }
 
-    let placeholder = "https://www.youtube.com/watch?v=zn5sTDXSp8E";
+    let _placeholder = "https://www.youtube.com/watch?v=zn5sTDXSp8E";
     let args = match Args::get().parse() {
         Ok(parsed) => parsed,
         Err(err) => {
@@ -33,7 +33,87 @@ async fn main() -> std::process::ExitCode {
     }
     match args.method.clone().expect("Failed to catch invalid method early") {
         args::types::Method::Get => {
-            println!("{:#?}", args);
+            let json = cobalt_args(&args);
+
+            let request = reqwest::Client::new().post("https://co.wuk.sh/api/json")
+                .header("User-Agent", &format!("tcobalt {}", VERSION.trim()))
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(json);
+
+            match request.send().await {
+                Ok(res) => {
+                    match json::parse(res.text().await.unwrap()) {
+                        Ok(json) => {
+                            match json.get("status".into()).unwrap().get_str().unwrap().as_str() {
+                                "error" => {
+                                    let text = json.get("text").unwrap().get_str().unwrap();
+                                    eprintln!("Cobalt returned error: {text}");
+                                    return std::process::ExitCode::FAILURE;
+                                },
+                                "stream" | "redirect" => {
+                                    let url = json.get("url").unwrap().get_str().unwrap();
+                                    let stream_request = reqwest::Client::new().get(url)
+                                        .header("User-Agent", &format!("tcobalt {}", VERSION.trim()));
+                                    
+                                    match stream_request.send().await {
+                                        Ok(res) => {
+                                            let filename = match args.out_filename {
+                                                Some(name) => name,
+                                                None => {
+                                                    let disposition = res.headers().get("Content-Disposition").unwrap().to_str().unwrap();
+                                                    let mut pass: u8 = 0;
+                                                    let mut filename = String::new();
+                                                    for c in disposition.chars() {
+                                                        if c == ';' || c == '\"' {
+                                                            pass += 1;
+                                                            continue;
+                                                        }
+                                                        if pass == 2 {
+                                                            filename.push(c);
+                                                        }
+                                                    }
+                                                    filename
+                                                }
+                                            };
+                                            match res.bytes().await {
+                                                Ok(stream) => {
+                                                    let path = std::env::current_dir().unwrap().join(filename.clone());
+                                                    match std::fs::write(path, stream) {
+                                                        Ok(_) => {
+                                                            eprintln!("File downloaded successfully! {filename}");
+                                                        },
+                                                        Err(e) => {
+                                                            eprintln!("Unable to write data to file: {}", e.to_string());
+                                                            return std::process::ExitCode::FAILURE;
+                                                        }
+                                                    }
+                                                },
+                                                Err(e) => {
+                                                    eprintln!("Error decoding byte stream: {}", e.to_string());
+                                                    return std::process::ExitCode::FAILURE;
+                                                }
+                                            }
+                                        },
+                                        Err(e) => {
+                                            eprintln!("Live renderer did not respond: {}", e.to_string());
+                                            return std::process::ExitCode::FAILURE;
+                                        }
+                                    }
+                                },
+                                _ => unimplemented!()
+                            }
+                        },
+                        Err(e) => {
+                            print_cobalt_error(e);
+                            return std::process::ExitCode::FAILURE;
+                        }
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Cobalt server did not respond: {}", e.to_string());
+                }
+            }
         },
         args::types::Method::Bulk => {
             println!("{:#?}", args);
@@ -54,9 +134,7 @@ async fn main() -> std::process::ExitCode {
             let stats = match json::parse(ver/* .clone() */) {
                 Ok(j) => j,
                 Err(e) => {
-                    eprintln!("Cobalt server returned improper JSON");
-                    eprintln!("JSON parse error: {e}");
-                    eprintln!("Either Cobalt is down, or you somehow got blocked specifically in this application.\n");
+                    print_cobalt_error(e);
                     // eprintln!("Cobalt returned response: {:?}", ver);
                     return std::process::ExitCode::FAILURE;
                 }
@@ -69,6 +147,37 @@ async fn main() -> std::process::ExitCode {
         },
     }
     std::process::ExitCode::SUCCESS
+}
+
+fn print_cobalt_error(error: String) {
+    eprintln!("Cobalt server returned improper JSON");
+    eprintln!("JSON parse error: {error}");
+    eprintln!("Either Cobalt is down, or you somehow got blocked specifically in this application.\n");
+}
+
+const POST_TEMPLATE: &str = "{
+    \"url\": \"<url>\",
+    \"vCodec\": \"<vcodec>\",
+    \"vQuality\": \"<vquality>\",
+    \"aFormat\": \"<aformat>\",
+    \"filenamePattern\": \"classic\",
+    \"isAudioOnly\": <audio-only>,
+    \"isTTFullAudio\": false,
+    \"isAudioMuted\": <audio-muted>,
+    \"dubLang\": false,
+    \"disableMetadata\": false,
+    \"twitterGif\": <twitter-gif>,
+    \"vimeoDash\": false
+}";
+fn cobalt_args(args_in: &Args) -> String {
+    POST_TEMPLATE.to_string()
+        .replace("<url>", &args_in.c_url.clone().unwrap())
+        .replace("<vcodec>", &args_in.c_video_codec.print())
+        .replace("<vquality>", &args_in.c_video_quality.to_string())
+        .replace("<aformat>", &args_in.c_audio_format.print())
+        .replace("<audio-only>", &args_in.c_audio_only.to_string())
+        .replace("<audio-muted>", &args_in.c_audio_muted.to_string())
+        .replace("<twitter-gif>", &args_in.c_twitter_gif.to_string())
 }
 
 #[cfg(test)]
